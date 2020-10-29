75856d860998b77bdb03e3dc709b0856d322f2f5
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2020-10-29 15:02:04 +0000

    add all theme colors

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -6,16 +6,23 @@
  */
 
 import React from 'react';
-
 import InitialReleaseScreen from './screens/InitialReleaseScreen';
+import TestScreen from './screens/TestScreen';
 import AppStack from './navigation/AppStack';
 
 export default function AppContainer() {
   return (
     <AppStack.Navigator>
-      <AppStack.Screen
+      {/* <AppStack.Screen
         name="Initial Release"
         component={InitialReleaseScreen}
+        options={{
+          headerShown: false,
+        }}
+      /> */}
+      <AppStack.Screen
+        name="Test"
+        component={TestScreen}
         options={{
           headerShown: false,
         }}

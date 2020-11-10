51f4e363bfc749576ecc797c37cf630cc3ba65b0
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2020-11-10 14:26:43 +0000

    feat(PDL-180) add dropdown to language selection screen

--- src/screens/LanguageSelectionScreen.js
+++ src/screens/LanguageSelectionScreen.js
@@ -7,17 +7,19 @@
  */
 
 import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {View, Image} from 'react-native';
 import {ScaleHook} from 'react-native-design-to-component';
 import useTheme from '../hooks/theme/UseTheme';
 import useDictionary from '../hooks/localisation/useDictionary';
 import DefaultButton from '../components/Buttons/DefaultButton';
+import {Form, FormHook} from 'the-core-ui-module-tdforms';
+import TDIcon from 'the-core-ui-component-tdicon';
 
 const splashImage = require('../../assets/images/splash.png');
 
 export default function LanguageSelectionScreen() {
   // ** ** ** ** ** SETUP ** ** ** ** **
-  const {getHeight, getWidth, fontSize, radius} = ScaleHook();
+  const {getHeight, fontSize} = ScaleHook();
   const {colors, textStyles} = useTheme();
   const {dictionary} = useDictionary();
   const {
@@ -26,9 +28,14 @@ export default function LanguageSelectionScreen() {
     DropdownText_SelectLanguage,
     DropdownText_Urdu,
   } = dictionary;
+  const dropdownData = [
+    DropdownText_English,
+    DropdownText_Hindi,
+    DropdownText_Urdu,
+  ];
 
   // ** ** ** ** ** STYLES ** ** ** ** **
-  const styles = StyleSheet.create({
+  const styles = {
     container: {
       alignItems: 'center',
       height: '100%',
@@ -40,8 +47,40 @@ export default function LanguageSelectionScreen() {
     },
     buttonContainer: {
       marginBottom: getHeight(40),
+      height: getHeight(150),
+      width: '100%',
+      alignItems: 'center',
     },
-  });
+    iconStyle: {
+      size: fontSize(12),
+      solid: true,
+    },
+    labelText: {
+      ...textStyles.medium14_brownishGrey100,
+    },
+  };
+
+  const cells = [
+    {
+      name: 'language',
+      type: 'dropdown',
+      label: DropdownText_SelectLanguage,
+      labelTextStyle: styles.labelText,
+      placeholder: DropdownText_English,
+      data: dropdownData,
+      rightAccessory: () => (
+        <View style={styles.iconContainer}>
+          <TDIcon input="chevron-down" inputStyle={styles.iconStyle} />
+        </View>
+      ),
+      iconTintColor: colors.black100,
+    },
+  ];
+  const config = {
+    editedColor: colors.brownishGrey100,
+    inactiveColor: colors.brownishGrey100,
+    activeColor: colors.brownishGrey100,
+  };
 
   // ** ** ** ** ** FUNCTIONS ** ** ** ** **
   // ** ** ** ** ** RENDER ** ** ** ** **
@@ -49,6 +88,7 @@ export default function LanguageSelectionScreen() {
     <View style={styles.container}>
       <Image source={splashImage} style={styles.image} />
       <View style={styles.buttonContainer}>
+        <Form {...{cells, config}} />
         <DefaultButton type="setLanguage" variant="white" />
       </View>
     </View>

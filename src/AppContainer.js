78258ef083c84ff9a3b8ad0bdce571d4dc1c3a93
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2020-11-19 10:41:04 +0000

    finish fix calendar

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -9,18 +9,10 @@ import React from 'react';
 import {AppStack} from './navigation';
 import AuthContainer from './routes/AuthContainer';
 import TabContainer from './routes/TabContainer';
-import ProgressScreen from './screens/progress/ProgressScreen';
 
 export default function AppContainer() {
   return (
     <AppStack.Navigator headerMode="screen">
-      <AppStack.Screen
-        name="Progress"
-        component={ProgressScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
       <AppStack.Screen
         name="AuthContainer"
         component={AuthContainer}

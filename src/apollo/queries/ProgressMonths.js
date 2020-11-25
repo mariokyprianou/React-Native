50347ba760fcc9eb7a814520a1bc5e2925a0f6fa
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2020-11-25 15:11:49 +0000

    feat(PDL-150) progress months query

---
+++ src/apollo/queries/ProgressMonths.js
@@ -0,0 +1,34 @@
+/*
+ * Jira Ticket:
+ * Created Date: Wed, 25th Nov 2020, 14:55:35 pm
+ * Author: Jodi Dublon
+ * Email: jodi.dublon@thedistance.co.uk
+ * Copyright (c) 2020 The Distance
+ */
+
+import gql from 'graphql-tag';
+
+export default gql`
+  query ProgressMonths {
+    progressMonths {
+      progress {
+        startOfMonth
+        days {
+          id
+          date
+          type
+        }
+      }
+      progressHistory {
+        progress {
+          startOfMonth
+          days {
+            id
+            date
+            type
+          }
+        }
+      }
+    }
+  }
+`;

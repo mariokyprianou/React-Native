bf7b7478ef551eba4a62be8c0b122adc4be3a09f
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2020-11-25 15:45:44 +0000

    feat(PDL-165) upload progress image mutation

---
+++ src/apollo/queries/ProgressImages.js
@@ -0,0 +1,18 @@
+/*
+ * Jira Ticket:
+ * Created Date: Wed, 25th Nov 2020, 15:33:50 pm
+ * Author: Jodi Dublon
+ * Email: jodi.dublon@thedistance.co.uk
+ * Copyright (c) 2020 The Distance
+ */
+
+import gql from 'graphql-tag';
+
+export default gql`
+  query ProgressImages {
+    progressImages {
+      takenAt
+      imageUrl
+    }
+  }
+`;

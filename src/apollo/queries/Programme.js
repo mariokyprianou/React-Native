79fa6583e37b00cdeff76060b7fbe6b4d9d451b5
Christos Demetriou <christos.demetiou@thedistance.co.uk>
2021-01-19 12:44:50 +0200

    PDL-153 Added getProgramme query

---
+++ src/apollo/queries/Programme.js
@@ -0,0 +1,86 @@
+/*
+ * Created Date: Tue, 19th Jan 2021, 12:27:27 pm
+ * Author: Christos Demetriou
+ * Email: christos.demetiou@thedistance.co.uk
+ * Copyright (c) 2021 JM APP DEVELOPMENT LTD
+ */
+
+import gql from 'graphql-tag';
+
+export default gql`
+  query Programme {
+    getProgramme {
+      id
+      trainer {
+        id
+        name
+      }
+      environment
+      programmeImage
+      currentWeek {
+        weekNumber
+        workouts {
+          id
+          orderIndex
+          name
+          overviewImage
+          intensity
+          duration
+          completedAt
+          exercises {
+            id
+            orderIndex
+            exercise {
+              id
+              name
+              coachingTips
+              video
+              videoEasy
+              videoEasiest
+              weight
+            }
+            sets {
+              setNumber
+              quantity
+              restTime
+            }
+            setType
+            notes
+          }
+        }
+      }
+      nextWeek {
+        weekNumber
+        workouts {
+          id
+          orderIndex
+          name
+          overviewImage
+          intensity
+          duration
+          completedAt
+          exercises {
+            id
+            orderIndex
+            exercise {
+              id
+              name
+              coachingTips
+              video
+              videoEasy
+              videoEasiest
+              weight
+            }
+            sets {
+              setNumber
+              quantity
+              restTime
+            }
+            setType
+            notes
+          }
+        }
+      }
+    }
+  }
+`;

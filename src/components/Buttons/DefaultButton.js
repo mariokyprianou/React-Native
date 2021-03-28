87df771699ec353776595ac89d7ab9421a2468c2
Christosdemet <christos.demetiou@thedistance.co.uk>
2021-03-28 21:56:05 +0300

    PDL-751 Disable button for 1 second after press

--- src/components/Buttons/DefaultButton.js
+++ src/components/Buttons/DefaultButton.js
@@ -6,7 +6,7 @@
  * Copyright (c) 2020 The Distance
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {ScaleHook} from 'react-native-design-to-component';
 import useTheme from '../../hooks/theme/UseTheme';
@@ -34,6 +34,8 @@ export default function DefaultButton({
 
   const reminderIcon = require('../../../assets/icons/reminder.png');
   const shareIcon = require('../../../assets/icons/share.png');
+
+  const [pressed, setPressed] = useState(false);
 
   const {ButtonDict} = dictionary;
 
@@ -259,6 +261,18 @@ export default function DefaultButton({
       ? text.toUpperCase()
       : text;
 
+  useEffect(()=> {
+    if (pressed) {
+      setTimeout(() => setPressed(false), 1000);
+    }
+
+  }, [pressed]);
+
+  const handlePress = () => {
+    setPressed(true);
+    onPress();
+  }
+ 
   const renderButtonText = () => {
     if (customSubtext) {
       return (
@@ -282,7 +296,7 @@ export default function DefaultButton({
   if (variant === 'gradient') {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={onPress} style={styles.touch}>
+        <TouchableOpacity onPress={pressed ? null : handlePress} style={styles.touch}>
           <LinearGradient
             style={styles.gradient}
             start={{x: 0, y: 0}}
@@ -311,7 +325,7 @@ export default function DefaultButton({
   if (type === 'continueFromWeek') {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={onPress} style={styles.touch}>
+        <TouchableOpacity onPress={pressed ? null : handlePress} style={styles.touch}>
           <Text style={styles.text}>{`${buttonText[type]} ${weekNo}`}</Text>
           <View style={styles.iconContainer}>
             <TDIcon
@@ -327,7 +341,7 @@ export default function DefaultButton({
   if (type === 'programme') {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={onPress} style={styles.touch}>
+        <TouchableOpacity onPress={pressed ? null : handlePress} style={styles.touch}>
           <Text
             style={
               styles.text
@@ -347,7 +361,7 @@ export default function DefaultButton({
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={onPress}
+        onPress={pressed ? null : handlePress}
         style={styles.touch}
         disabled={disabled}>
         {renderButtonText()}

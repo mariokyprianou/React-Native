95ccc3da37b28c3840f62a1f86e57c67620ee45a
James Shaw <james.shaw@thedistance.co.uk>
2021-06-01 12:44:00 +0100

    [PDL-1040] Fixed loading spinner issue.

--- src/hooks/data/ProgressDataProvider.js
+++ src/hooks/data/ProgressDataProvider.js
@@ -92,8 +92,6 @@ export default function DataProvider(props) {
       }
 
       images = [].concat(formattedImages, images);
-
-      console.log('NEWIMAGES', images.length);
       setUserImages(images);
     },
     [userImages],
